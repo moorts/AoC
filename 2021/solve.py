7812ad8d70738453bab667f7011a52542b5d9a58
moorts <moritz.gutfleisch@gmx.de>
2021-12-11 12:37:16 +0100

    Template

---
+++ 2021/solve.py
@@ -0,0 +1,59 @@
+import subprocess
+
+import numpy as np
+from functools import *
+from parse import parse
+
+def ints_it(it):
+    while it:
+        yield int(it.pop(0))
+
+def ints(it):
+    return list(ints_it(it))
+
+""" Interpreter code (just in case)
+    x = 0
+    depth = 0
+    aim = 0
+    for inst in data:
+        opc, arg = inst.split(" ")
+        if opc == "forward":
+            x += int(arg)
+            depth += aim * int(arg)
+        elif opc == "down":
+            aim += int(arg)
+        elif opc == "up":
+            aim -= int(arg)
+"""
+
+with open('./input') as f:
+    #data = f.read() # raw data
+    data = f.read().splitlines() # lines
+    #data = f.read().split("\n\n") # Paragraphs
+
+test_data = """\
+"""
+
+
+test_data = test_data.splitlines()
+#test_data = test_data
+#test_data = test_data.split("\n\n")
+
+
+def solve(data):
+    pass
+
+
+
+def solve2(data):
+    pass
+
+first_result = solve(test_data)
+second_result = solve2(test_data)
+print(f"TESTDATA Part 1: {first_result}")
+print(f"TESTDATA Part 2: {second_result}")
+
+first_result = solve(data)
+second_result = solve2(data)
+print(f"Part 1: {first_result}")
+print(f"Part 2: {second_result}")
